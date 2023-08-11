84457780db4eafbb3121fea337111755f2310033
yaxche-web <eaqmijangos@gmail.com>
2023-08-12 01:00:51 +0200

    url

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-
+import App from './App.jsx';
 
 
 const rootElement = document.getElementById('root');
@@ -10,5 +9,3 @@ createRoot(rootElement).render(
     <App />
   </React.StrictMode>
 );
-
-
