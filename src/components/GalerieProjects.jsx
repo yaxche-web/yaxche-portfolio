d8a1572a550d45af6b626291cc5dd2b2780b83ec
yaxche-web <eaqmijangos@gmail.com>
2023-08-12 01:16:29 +0200

    urlb

--- src/components/GalerieProjects.jsx
+++ src/components/GalerieProjects.jsx
@@ -13,34 +13,28 @@ function GalerieProjects() {
 
   return (
     <div className="galerie">
-        {projectsData.map((project) => (
-            <div key={project.id} className={`project ${expandedProjectId === project.id ? 'open' : ''}`}>
+      {projectsData.map((project) => (
+        <div key={project.id} className={`project ${expandedProjectId === project.id ? 'open' : ''}`}>
+          <h3>{project.titre}</h3>
+          <img src={`${process.env.PUBLIC_URL}/${project.image}`} alt={project.titre} />
 
-                <h3>{project.titre}</h3>
-
-                <img src={`images/${project.image}`} alt={project.titre} />
-
-                <div className="techno">
-                  {project.techno.map((tech, index) => (
-                    <span key={index}>{tech}</span>
-                  ))}
-                </div>
-
-                <button className="en-savoir-plus" onClick={() => toggleProjectInfo(project.id)}>{expandedProjectId === project.id ? 'Fermer' :'En savoir plus'}</button>
-
-                {expandedProjectId === project.id && (
-                  <div className="more-info">
-                      <p>{project.description}</p>
-
-                      <p><strong>Problématiques et solutions:</strong> {project["problématique et solution"]}</p>
-
-                      <p><strong>Compétences développées:</strong> {project["compétences développées"]}</p>
-
-                      <p><a href={project.githubLink} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a></p>
-                      
-                  </div>
-          )}
+          <div className="techno">
+            {project.techno.map((tech, index) => (
+              <span key={index}>{tech}</span>
+            ))}
+          </div>
+          <button className="en-savoir-plus" onClick={() => toggleProjectInfo(project.id)}>
+            {expandedProjectId === project.id ? 'Fermer' : 'En savoir plus'}
+          </button>
+          {expandedProjectId === project.id && (
+            <div className="more-info">
+              <p>{project.description}</p>
+              <p><strong>Problématiques et solutions:</strong> {project["problématique et solution"]}</p>
+              <p><strong>Compétences développées:</strong> {project["compétences développées"]}</p>
+              <p><a href={project.githubLink} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a></p>
             </div>
+          )}
+        </div>
       ))}
     </div>
   );
